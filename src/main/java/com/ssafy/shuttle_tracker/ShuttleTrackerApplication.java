da31ca851d8545bb0fb428e0bacbd0ce49a95a17
jsg998877 <jsg998877@gmail.com>
2025-10-24 18:02:34 +0900

    init

---
+++ src/main/java/com/ssafy/shuttle_tracker/ShuttleTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.shuttle_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShuttleTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShuttleTrackerApplication.class, args);
+	}
+
+}
